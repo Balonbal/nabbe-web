bec7935d853b44457c64466753eb7cfce62619d3
Olav Bech Bråten <olavbb@hotmail.com>
2017-06-12 14:28:55 +0200

    Code cleanup

--- public_html/index.php
+++ public_html/index.php
@@ -2,17 +2,10 @@
 session_start();
 include_once realpath(dirname(__FILE__)) . "/../library/config/configuration.php";
 include_once TEMPLATES_PATH . "/header.php";
-if (isset($_SESSION["nabbe-jwt"])):?>
-<script type="text/javascript">
-    var store = store || {};
-
-    //You can now make authorized requests in pure js, nice :)
-    store.JWT = <?=$_SESSION["nabbe-jwt"]?>;
-    $.ajaxSetup({
-        headers: {"Authorization": "Bearer " + store.JWT.jwt}
-    });
-</script>
-<?php endif; ?>
+//Avoid unexpected behaviour
+unset($_SESSION["service"]);
+unset($_SESSION["user_id"]);
+?>
 <body>
 <?php include_once TEMPLATES_PATH . "/navbar.php"; ?>
 <div id="content">
@@ -21,23 +14,8 @@ if (isset($_SESSION["nabbe-jwt"])):?>
     <?php else: ?>
     <div class="container">
         <div class="row">
-            <div class="col-md-offset-3 col-md-6 social-buttons">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        Please log in to continue
-                    </div>
-                    <div class="panel-body">
-                        <a class="btn btn-large btn-block btn-social btn-facebook" href="<?php include_once LIBRARY_PATH . "/fb-login.php"; ?>">
-                            <span class="fa fa-facebook"></span>Continue with Facebook
-                        </a>
-                        <a class="btn btn-large btn-block btn-social btn-google" href="<?php include_once LIBRARY_PATH . "/google-login.php"; ?>">
-                            <span class="fa fa-google"></span>Contiue with Google
-                        </a>
-                    </div>
-                </div>
-                <div class="alert alert-info">
-                    <strong>Note:</strong> We only use this for authentication, no user data is stored.
-                </div>
+            <div class="col-md-offset-3 col-md-6">
+                <?php include_once TEMPLATES_PATH . "/socialButtons.php" ?>
             </div>
         </div>
     </div>
