e1e2e95d0f7a23cc5d964a196e02849c030967bb
Tinus Flagstad <tinus@flagstad.as>
2017-06-13 20:30:40 +0200

    Created an about page (am i doing stuff right?)

---
+++ public_html/about/index.php
@@ -0,0 +1,24 @@
+<?php
+include_once realpath(dirname(__FILE__)) . "/../../library/config/configuration.php";
+include_once TEMPLATES_PATH . "/header.php";
+?>
+<body>
+<?php include_once TEMPLATES_PATH . "/navbar.php"; ?>,
+
+<div id="content">
+    <div class="container">            
+        <div class="col-md-2 col-md-offset-5">
+            <p>
+                <strong>Nabbe.io</strong> is a game created by:<br>
+                Olav Bech Bråten<br>
+                Tinus Flagstad<br>
+                Aksel Slettemark<br>
+                Håkon Wardeberg<br>
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php include_once TEMPLATES_PATH . "/footer.php"; ?>
+</body>
+</html>
