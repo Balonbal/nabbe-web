922911ec67fad20c088ac3d1fe8e97bea09c2d00
Tinus Flagstad <tinus@flagstad.as>
2017-06-15 15:55:35 +0200

    Fixed a bug on about where panels would sometimes get a different height.

--- public_html/about/index.php
+++ public_html/about/index.php
@@ -13,10 +13,10 @@ include_once TEMPLATES_PATH . "/header.php";
             </h3>
             <div class="col-sm-3 col-xs-6">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
+                    <div class="panel-heading fixed-panel">
                         <h3 class="panel-title text-center">Olav Bech Bråten</h3>
                     </div>
-                        <div class="panel-body">
+                    <div class="panel-body fixed-panel">
                         Our master
                     </div>
                 </div>
@@ -24,10 +24,10 @@ include_once TEMPLATES_PATH . "/header.php";
 
             <div class="col-sm-3 col-xs-6">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
+                    <div class="panel-heading fixed-panel">
                         <h3 class="panel-title text-center">Tinus Flagstad</h3>
                     </div>
-                        <div class="panel-body">
+                    <div class="panel-body fixed-panel">
                         our slave
                     </div>
                 </div>
@@ -35,10 +35,10 @@ include_once TEMPLATES_PATH . "/header.php";
 
             <div class="col-sm-3 col-xs-6">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
+                    <div class="panel-heading fixed-panel">
                         <h3 class="panel-title text-center">Aksel Slettemark</h3>
                     </div>
-                        <div class="panel-body">
+                    <div class="panel-body fixed-panel">
                         backend master
                     </div>
                 </div>
@@ -46,10 +46,10 @@ include_once TEMPLATES_PATH . "/header.php";
 
             <div class="col-sm-3 col-xs-6">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
+                    <div class="panel-heading fixed-panel">
                         <h3 class="panel-title text-center">Håkon Wardeberg</h3>
                     </div>
-                        <div class="panel-body">
+                    <div class="panel-body fixed-panel">
                         our krenger-kreiger
                     </div>
                 </div>
