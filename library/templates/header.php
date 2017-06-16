437fc684642020b93702bcd8ecd7ab8e6bc16354
Olav Bech Bråten <olavbb@hotmail.com>
2017-06-16 17:16:13 +0200

    Merged pageloader

--- library/templates/header.php
+++ library/templates/header.php
@@ -7,5 +7,20 @@
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="bootstrap/dist/css/bootstrap-social.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <title>NAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABE</title>
+    <script src="http://<?= $_SERVER["HTTP_HOST"] ?>/js/network.js"></script>
+    <?php if (isset($page) && file_exists(LIBRARY_PATH . "../public_html/js/" . $page . ".js")):?>
+    <script src="http://<?=$_SERVER["HTTP_HOST"]?>/js/<?=$page?>.js"></script>
+    <?php endif?>
+    <?php if (isset($_SESSION["nabbe-jwt"])):?>
+    <script type="text/javascript">
+        var store = store || {};
+
+        //You can now make authorized requests in pure js, nice :)
+        store.JWT = <?=$_SESSION["nabbe-jwt"]?>;
+        $.ajaxSetup({
+            headers: {"Authorization": "Bearer " + store.JWT.jwt}
+        });
+    </script>
+    <?php endif; ?>
+    <title>Nabbe - <?=$_SERVER["PHP_SELF"] ?></title>
 </head>
--- public_html/index.php
+++ public_html/index.php
@@ -1,30 +1,28 @@
 <?php
 include_once realpath(dirname(__FILE__)) . "/../library/config/configuration.php";
+//Avoid unexpected behaviour
+unset($_SESSION["service"]);
+unset($_SESSION["user_id"]);
+
+$page = "index"; //Default
+
+if (isset($_REQUEST["page"])) {
+    $page=$_REQUEST["page"];
+}
+
+$fullpath = PAGES_PATH . "/" . $page . ".php";
+//Our special 404 page
+if (!file_exists($fullpath)) $fullpath = PAGES_PATH . "/404page.php";
+
 include_once TEMPLATES_PATH . "/header.php";
-?>
-<body>
-<?php include_once TEMPLATES_PATH . "/navbar.php"; ?>
-<div id="content">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-offset-4 col-sm-4 social-buttons">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Please log in to continue
-                    </div>
-                    <div class="panel-body">
-                        <a class="btn btn-large btn-block btn-social btn-facebook" href="<?php include_once LIBRARY_PATH . "/fb-login.php"; ?>">
-                            <span class="fa fa-facebook"></span>Continue with Facebook
-                        </a>
-                        <a class="btn btn-large btn-block btn-social btn-google" href="<?php include_once LIBRARY_PATH . "/google-login.php"; ?>">
-                            <span class="fa fa-google"></span>Contiue with Google
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<?php include_once TEMPLATES_PATH . "/footer.php"; ?>
+echo "<body>";
+include_once TEMPLATES_PATH . "/navbar.php";
+
+if (file_exists($fullpath)) {
+    include_once $fullpath;
+}
+//Do something else? -- should give our 404 page
+
+include_once TEMPLATES_PATH . "/footer.php"; ?>
 </body>
 </html>
