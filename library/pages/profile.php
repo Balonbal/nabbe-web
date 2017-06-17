4e4d8e57ed7de506dea1108d3d31cbb2bcdc8875
Olav Bech Bråten <olavbb@hotmail.com>
2017-06-17 16:00:30 +0200

    Changed profile page to comply with page-fetcher

---
+++ library/pages/profile.php
@@ -0,0 +1,55 @@
+<div id="content">
+    <div class="container">
+        <div class="row">
+            <?php if (isset($_SESSION["nabbe-jwt"])): ?>
+                <p>
+                    <div class="col-sm-6 col-xs-12">
+                        <h3>
+                            Stats for <span id="oldUsername"><?=decode_JWT(json_decode($_SESSION["nabbe-jwt"])->jwt)->sub;?></span>
+                        </h3>
+                </p>
+                <p>You have in total x wins and y defeats</p>
+                <p>That is a x/y * 100 % winrate!</p>
+                <p>Previous 10 matches:</p>
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-success text-center">
+                        You defeated <a href="http://nabbe.gabeorama.org/profile/sly">sly</a> with a score of 16-2
+                    </li>
+                    <li class="list-group-item list-group-item-danger text-center">
+                        You lost against <a href="http://nabbe.gabeorama.org/profile/sly">sly</a> with a score of 13-16
+                    </li>
+                    <li class="list-group-item list-group-item-warning text-center">
+                        You tied against <a href="http://nabbe.gabeorama.org/profile/sly">sly</a> with a score of 16-16
+                    </li>
+                </ul>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <h3> Account Settings </h3>
+                    <div id="divSettingsBoxes">
+                        <!-- Probably have to change new_user with change_user.php -->
+                        <form action="../new_user.php" method="POST" id="newUserForm" class="form-horizontal">
+                        <div class="form-group has-feedback">
+                                <input type="text" maxlength="16" minlength="1" pattern="^[_A-z0-9]{1,16}" data-remote="/api/users.php" class="form-control" name="username" placeholder="New Username" required>
+                                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                            <div class="help-block with-errors col-sm-offset-2">Please pick a alphanumeric username (1-16 chars)</div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-4 col-sm-10">
+                                <button type="submit" class="btn btn-success">Change Username</button>
+                            </div>
+                        </div>
+
+                    <div id="divSettingsBoxes">
+                        <div class="col-sm-offset-4 col-sm-10">
+                            <button type="button" class="btn btn-danger" id="btnDelete">Delete Account</button>
+                        </div>
+                    </div>
+                </div>
+            <?php else: ?>
+                <div class="col-md-offset-3 col-md-6">
+                    <?php include_once TEMPLATES_PATH . "/socialButtons.php" ?>
+            <?php endif ?>
+        </div>
+    </div>
+</div>
---
+++ public_html/js/profile.js
@@ -0,0 +1,36 @@
+//Buttons
+const btnDelete = document.getElementById("btnDelete");
+const btnChangeUN = document.getElementById("btnChangeUN");
+const btnChangePW = document.getElementById("btnChangePW");
+
+//Input fields
+const inputUsername = document.getElementById("inputUsername");
+const inputPasswordOld = document.getElementById("inputPasswordOld");
+const inputPasswordNew1 = document.getElementById("inputPasswordNew1");
+const inputPasswordNew2 = document.getElementById("inputPasswordNew2");
+
+//I know it's a really shady way to get the old username, but I don't know how else I could do this.
+let oldUsername = document.getElementById("oldUsername").innerHTML;
+
+btnDelete.addEventListener('click', function (e) {
+	if (confirm("Are you sure you want to permanently delete your account? (Note: this can not be reversed.)")) {
+    	//Delete shit
+    	console.log("delete shit");
+	} else {
+	    // Do nothing.
+	    console.log("dont delete shit");
+	}
+});
+
+btnChangeUN.addEventListener('click', function (e) {
+	if (inputUsername.value === "" || inputUsername.value === oldUsername) {
+		alert("Your new username can not be the same or blank.");
+	} else {
+		if (confirm("Are you sure you want to change your name to " + inputUsername.value + "?")) {
+			oldUsername = inputUsername.value;
+	    	console.log("Change name");
+		} else {
+		    console.log("dont change name");
+		}
+	}
+});
