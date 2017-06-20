753077caae855890fc286be8d67564e87dc6cae5
Tinus Flagstad <tinus@flagstad.as>
2017-06-20 19:00:22 +0200

    Added a template and changed some stuff, this is still very much a WIP.

--- library/pages/play.php
+++ library/pages/play.php
@@ -1,14 +1,32 @@
+<!--
 <div id="content">
     <div class="container">
         <div class="row">
             <div class="col-sm-6 col-xs-12">
                 <h3>Start a new match!</h3>
-                <!-- Use the same friendslist as in /add/ -->
+                <div id="currentMatches" class="list-group">
+
+                    <div style="height: 41px">
+                        <a href="ice_poseidon" class="list-group-item" style="width: 100%; float: left">
+                            <span>Start a match against ice_poseidon</span>
+                        </a>
+                        <a href="cx_homie" class="list-group-item" style="width: 100%; float: left">
+                            <span>Start a match against cx_homie</span>
+                        </a>
+                        <a href="dawg" class="list-group-item" style="width: 100%; float: left">
+                            <span>Start a match against dawg</span>
+                        </a>
+                        <a href="dog" class="list-group-item" style="width: 100%; float: left">
+                            <span>Start a match against dog</span>
+                        </a>
+                    </div>
+
+                </div>
             </div>
 
             <div class="col-sm-6 col-xs-12">
                 <h3>Current matches:</h3>
-                <div class="list-group">
+                <div id="currentMatches" class="list-group">
 
                     <div style="height: 41px">
                         <a href="Sly" class="list-group-item" style="width: 100%; float: left">
@@ -41,6 +59,75 @@
                     </div>
 
                 </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+<div id="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3 col-md-2 sidebar">
+                <ul class="nav nav-sidebar">
+                    <li class="active"><a href="#">Start a new match<span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Reports</a></li>
+                    <li><a href="#">Analytics</a></li>
+                    <li><a href="#">Export</a></li>
+                </ul>
+                <ul class="nav nav-sidebar">
+                    <li><a href="">Nav item</a></li>
+                    <li><a href="">Nav item again</a></li>
+                    <li><a href="">One more nav</a></li>
+                    <li><a href="">Another nav item</a></li>
+                    <li><a href="">More navigation</a></li>
+                </ul>
+                <ul class="nav nav-sidebar">
+                    <li><a href="">Nav item again</a></li>
+                    <li><a href="">One more nav</a></li>
+                    <li><a href="">Another nav item</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                <h1 class="page-header">Match against Sly</h1>
+                <h3>Question 2/8</h3>
+
+                <h2 class="sub-header">Which song contains these lyrics?</h2>
+                <div class="jumbotron col-sm-6 col-xs-12">
+                    <p class="lead text-center">Wake up<br>
+                                Grab a brush and put a little (makeup)<br>
+                                Grab a brush and put a little<br>
+                </div>
+
+
+                <div class="row placeholders">
+                    <div class="col-sm-6 col-xs-12">
+                        <div class="col-xs-6 col-sm-6 placeholder">
+                            <!-- Do we want pictures for each alternative? or maybe we could make them all like questionmarks and have an animation when you choose one of the alternatives. turn them into like a checkmark if it was right or something? idk fam. -->
+                            <p>?</p>
+                            <!--
+                            <img src="http://www.clker.com/cliparts/d/c/0/d/1259089297665068165question%20mark%20clkerdotcom-md.png" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+                            -->
+                            <h4>Alternative 1</h4>
+                            <span class="text-muted">Chop Suey</span>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 placeholder">
+                            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+                            <h4>Alternative 2</h4>
+                            <span class="text-muted">Numb</span>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 placeholder">
+                            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+                            <h4>Alternative 3</h4>
+                            <span class="text-muted">Baby</span>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 placeholder">
+                            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+                            <h4>Alternative 4</h4>
+                            <span class="text-muted">Ora jeska bena</span>
+                        </div>
+                    </div>
+                </div>
+            <h2 class="sub-header">Section title</h2>
+                <p> need help? report bug etc maybe?</p>
             </div>
         </div>
     </div>
