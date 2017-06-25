78dd56b9e3e5a01708c59ab917578aa40d814732
Olav Bech Bråten <olavbb@hotmail.com>
2017-06-25 12:54:10 +0200

    Added display for stats

---
+++ library/pages/stats.php
@@ -0,0 +1,5 @@
+<div id="content" class="container">
+    <div class="row text-center">
+        <h1 class="page-header">Daily active Users: <span class="active_users"></span></h1>
+    </div>
+</div>
